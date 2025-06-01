5c8ca2315b0250680c27d497145d23a9fb0b5542
Zoren Espiritu <108162124+Rhnzo12@users.noreply.github.com>
2025-06-01 21:35:54 +0800

    Update update_users_table.php

--- database/migrations/update_users_table.php
+++ database/migrations/update_users_table.php
@@ -7,14 +7,14 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable()->change(); // Make phone nullable
+           
         });
     }
 
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable(false)->change(); // Revert to not nullable
+           
         });
     }
 }
